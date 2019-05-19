1e348fbe425b9dcd3ab0a6393146619ec68de1fe
Exdarface <exdarface@gmail.com>
2019-05-19 20:00:45 +0200

    Commentary Predateurs

--- fakarava/ecosystem/Predateurs.java
+++ fakarava/ecosystem/Predateurs.java
@@ -13,6 +13,9 @@ public class Predateurs extends Poissons {
     private static Integer BITE_FACTOR;
 
 
+    /**
+     * Fait chasser tous les Predateurs de la Lagune
+     */
     public static void chasse() {
         for (Case c : Lagune.grille) {
             if(c.getIs_passe()){ // Si la case est une passe
@@ -49,7 +52,7 @@ public class Predateurs extends Poissons {
                             index_prey = j;
                         }
                     }
-                    double surva = Math.max(0,list_prey[index_prey].getVivacite_proie()/list_prey[index_predator].getPoids_poisson()- Lagune.force_courant/100 );
+                    double surva = Math.max(0,list_prey[index_prey].getVivacite_proie()/list_pred[index_predator].getPoids_poisson()- Lagune.force_courant/100 );
                     // Calcul de la survie
                     if (rn.selection(surva)) {
                         // Création de la liste des AUTRES Predateurs
@@ -60,7 +63,7 @@ public class Predateurs extends Poissons {
                         
                         }
                         int index_mordre = rn.who(list_mordu.length); // Attaque aléatoire
-                        double q = list_pred[index_predator].getPoids_poisson()/BITE_FACTOR; // Dégats de l'attaque
+                        double q = list_mordu[index_mordre].getPoids_poisson()/BITE_FACTOR; // Dégats de l'attaque
                         // Mort du Predateurs si son poids est inférieur à q
                         if (list_mordu[index_mordre].getPoids_poisson() - q <= 0) {
                             c.removeContenu(list_mordu[index_mordre].getNumero_poisson());
@@ -77,6 +80,7 @@ public class Predateurs extends Poissons {
                     }
                     // Mort de la Proie
                     else{
+                        list_pred[index_predator].setPoids_poisson(list_pred[index_predator].getPoids_poisson()+list_prey[index_prey].getPoids_poisson());
                         c.removeContenu(list_prey[index_prey].getNumero_poisson());
                         for (Poissons p : c.getContenu()) {
                             if(p.getClass() == Proies.class){
@@ -85,7 +89,7 @@ public class Predateurs extends Poissons {
                         }
                     }
                 }
-                // Mise à jour des Poissons présents dans la Case
+                // Mise à jour des Poissons présents dans la Case en fin de chasse
                 Poissons[] list_final = {};
                 for (Predateurs p : list_pred) {
                     list_final[list_final.length] = ((Poissons)p);
@@ -106,6 +110,9 @@ public class Predateurs extends Poissons {
 
     //Méthodes d'Instances : 
 
+    /**
+     * Donne toutes les informations du Predateurs pointé
+     */
     @Override
     public String toString() {
         return this.getNumero_poisson()+":"+this.getClass().toString()+","+this.getNom_poisson()+","+
@@ -114,6 +121,9 @@ public class Predateurs extends Poissons {
 
     //Methodes de classe :
 
+    /**
+     * Fait se reproduire par clonage tous les Predateurs de la Lagune
+     */
     public static void se_reproduit() {
         if(unite_temps/PREDATOR_CLONE_TIME ==1){
             for (Case c : Lagune.grille) {
@@ -126,24 +136,43 @@ public class Predateurs extends Poissons {
         }
     }
 
+    /**
+     * Transforme le délai de reproduction des Predateurs
+     * @param PREDATOR_CLONE_TIME nouveau délai de reproduction
+     */
     public static void setPREDATOR_CLONE_TIME(Integer PREDATOR_CLONE_TIME) {
         Predateurs.PREDATOR_CLONE_TIME = PREDATOR_CLONE_TIME;
     }
 
+    /**
+     * Récupère le délai de reproduction des Predateurs
+     * @return délai de reproduction des Predateurs
+     */
     public static Integer getPREDATOR_CLONE_TIME() {
         return PREDATOR_CLONE_TIME;
     }
 
+    /**
+     * Transforme la puissance de la morsure des Predateurs
+     * @param BITE_FACTOR nouvelle puissance de la morsure des Predateurs
+     */
     public static void setBITE_FACTOR(Integer BITE_FACTOR) {
         Predateurs.BITE_FACTOR = BITE_FACTOR;
     }
 
+    /**
+     * Récupère la puissance de la morsure des Predateurs
+     * @return puissance de la morsure des Predateurs
+     */
     public static Integer getBITE_FACTOR() {
         return BITE_FACTOR;
     }
 
     // Méthodes implémentées :
 
+    /**
+     * Réduit le poids de 1 du Prédateur pointé
+     */
     @Override
     public void ticktock() {
         this.setPoids_poisson(this.getPoids_poisson()-1); // Réduire le poids
