963be9a279eeb3094bcaea49f5ca00f9cf3f8dbd
Exdarface <exdarface@gmail.com>
2019-05-25 17:13:50 +0200

    Itération 2 Rendu + Début Itération 3

--- fakarava/ecosystem/Camera.java
+++ fakarava/ecosystem/Camera.java
@@ -23,4 +23,28 @@ public class Camera extends Plongeurs {
     public String toString(){
         // TODO : Finir la méthode
     }
+
+    public void setDescription_poissons(String description_poissons) {
+        this.description_poissons = description_poissons;
+    }
+
+    public String getDescription_poissons() {
+        return description_poissons;
+    }
+
+    public void setId_camera(Point id_camera) {
+        this.id_camera = id_camera;
+    }
+
+    public Point getId_camera() {
+        return id_camera;
+    }
+
+    public void setDescription_chasse(String description_chasse) {
+        this.description_chasse = description_chasse;
+    }
+
+    public String getDescription_chasse() {
+        return description_chasse;
+    }
 }
